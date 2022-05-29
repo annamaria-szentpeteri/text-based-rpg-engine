8f2c7a766b15d9969929e2563014df4a6527c2af
Annamária Szentpéteri <szaea1994@gmail.com>
2022-05-29 18:16:24 +0200

    Base project structure (#2)

---
+++ text-based-rpg-engine/TBRPG.Designer.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TBRPG.Designer.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
